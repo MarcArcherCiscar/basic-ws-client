5e3abecf29d8346f53ce04148320bf03184308dc
Marc Archer <marc.archer@premiumnumbers.es>
2025-05-11 14:28:19 +0200

    feat: add server status display and connection listeners

--- src/socket-client.ts
+++ src/socket-client.ts
@@ -1,8 +1,24 @@
-import { io, Manager } from 'socket.io-client';
+import { io, Manager, Socket } from 'socket.io-client';
 
 export const connectToServer = () => {
     //localhost:3001/socket.io/socket.io.js
     const manager = new Manager('localhost:3001/socket.io/socket.io.js');
 
     const socket = manager.socket('/');
+
+    addListeners( socket );
+}
+
+const addListeners = ( socket: Socket ) => {
+    const serverStatusLabel = document.querySelector<HTMLSpanElement>('#server-status')!;
+
+    socket.on('connect', ()  => {
+        console.log('Connected to server');
+        serverStatusLabel.innerHTML = 'connected';
+    });
+
+    socket.on('disconnect', ()  => {
+        console.log('Disconnect to server');
+        serverStatusLabel.innerHTML = 'disconnected';
+    });
 }
